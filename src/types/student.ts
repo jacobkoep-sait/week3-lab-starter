51b4d1ece2f0f965292d8f627ab9269f1565ef36
AbimbolaPitan1 <toluabimbola.pitan@gmail.com>
2026-01-20 08:43:31 -0700

    Updated the student and staff data
    added data to the staff and students

--- src/types/student.ts
+++ src/types/student.ts
@@ -1,8 +1,12 @@
+import { Program } from "./program";
+import { Course } from "./course";
 // TODO: Create a type for student ids, which should be strings
+type StudentId = String;
 
 // TODO: Create a type for student statuses, which can either be "Full-time", or "Part-time"
-
+type StudentStatus = "Full-time" | "Part-time";
 // TODO: Create an interface for students
+
 // The interface should include the following properties:
 // an id (hint: use the StudentId type created above)
 // a name
@@ -16,4 +20,14 @@
 // an optional program id
 // an optional array of current course ids
 
-export interface Student {}
+export interface Student {
+  id: StudentId;
+  name: string;
+  year: number;
+  status: StudentStatus;
+  interest: String[];
+  githubUsername?: String;
+  funFact?: String;
+  ProgramId?: Program;
+
+}
