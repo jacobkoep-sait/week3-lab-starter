052a8e0b1f6cc8af5afb947f32730501d1327844
Leke Adeyemo <lekke.dev@gmail.com>
2026-01-26 10:08:51 -0700

    Created program interface and added program data

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -1,5 +1,6 @@
 import type { Course } from "../types/course";
 
 export const courses: Course[] = [
-  // add courses here
+  // add courses here{}
+  {},
 ];
--- src/types/program.ts
+++ src/types/program.ts
@@ -1,6 +1,9 @@
 // TODO: Create a type for program ids, which should be strings
+import { Delivery, School } from "./common";
+type ProgramId = string;
 
 // TODO: Create a type for credential types, which can either be "Diploma", "Degree", or "Certificate"
+type Credential = "Diploma" | "Degree" | "Certificate";
 
 // TODO: Create an interface for programs
 // The interface should include the following properties:
@@ -8,10 +11,21 @@
 // a name
 // a credential (hint: use the Credential type created above)
 // a school (hint: use the School type from common.ts)
+
 // a length in years
 // a number of credits
 // an optional delivery method (hint: use the Delivery type from common.ts)
 // an optional array of career paths
 // an optional note
 
-export interface Program {}
+export interface Program {
+  id: ProgramId;
+  name: string;
+  credential: Credential;
+  lengthOfyears: number;
+  numberOfCredits: number;
+  school: School;
+  delivery?: Delivery;
+  careerPath: string[];
+  note?: string;
+}
